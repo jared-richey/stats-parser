46e5053cc03956bf5fc0173218f266cc6305b62c
jared-richey <richeyj01@gmail.com>
2024-03-20 16:50:33 -0400

    Add favicon to web app

--- web_app/lines/urls.py
+++ web_app/lines/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from lines import views
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("soccer/", views.soccer, name="soccer"),
     path("basketball/", views.basketball, name="basketball"),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('/favicon.ico'))),
 ]
