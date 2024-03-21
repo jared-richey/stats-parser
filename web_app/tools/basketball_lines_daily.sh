6ed26a4a78ee9fa9e7fb033c669fbf19e171ede4
jared-richey <richeyj01@gmail.com>
2024-03-21 17:12:42 -0400

    Harden scripts & adjust cron job for UTC

--- web_app/tools/basketball_lines_daily.sh
+++ web_app/tools/basketball_lines_daily.sh
@@ -6,8 +6,21 @@ SCRIPT_PATH=$(readlink -f "$0")
 
 my_dir=$(dirname "$SCRIPT_PATH")
 
-echo "Running curl command to retrieve basketball stats" 
-curl -s -H "Content-type: text/html" https://www.espn.com/mens-college-basketball/lines > $my_dir/basketball_lines.html
+url="https://www.espn.com/mens-college-basketball/lines"
+input_file="$my_dir/basketball_lines.html"
 
-echo "Parsing basketball stats"
-python3 $my_dir/basketball_parser.py > $my_dir/basketball_stats.txt
+echo "Checking status code" 
+response_code=$(curl -s -o /dev/null -w "%{http_code}" -H "Content-type: text/html" $url)
+
+if [[ "$response_code" == 200 ]]; then
+    echo "Running curl command to retrieve basketball stats" 
+    curl -s -H "Content-type: text/html" $url > $input_file
+    if [[ -s "$input_file" ]]; then
+        echo "Parsing basketball stats"
+        python3 $my_dir/basketball_parser.py > $my_dir/basketball_stats.txt
+    else
+        echo "Input file doesn't exist"
+    fi
+else
+   echo "Request failed with non-200"
+fi
